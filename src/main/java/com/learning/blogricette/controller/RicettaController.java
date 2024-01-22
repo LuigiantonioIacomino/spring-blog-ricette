b0d8da1b35825b3d4529a68cfd88ae3f903612b8
LuigiantonioIacomino <iacominoluigi54@gmail.com>
2024-01-22 15:58:50 +0100

    modifica template

--- src/main/java/com/learning/blogricette/controller/RicettaController.java
+++ src/main/java/com/learning/blogricette/controller/RicettaController.java
@@ -1,6 +1,6 @@
 package com.learning.blogricette.controller;
-
 import com.learning.blogricette.model.Ricetta;
+import com.learning.blogricette.repository.RicettaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,20 +8,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
 import java.util.Optional;
+
 
 @Controller
 @RequestMapping("/ricetta")
 public class RicettaController {
-  @Autowired
-  private com.learning.blogricette.repository.ricettaRepository ricettaRepository;
 
-    @GetMapping
-    public String index(Model model) {
+@Autowired
+private RicettaRepository ricettaRepository;
+
+@GetMapping
+public String index(Model model) {
         model.addAttribute("ricettaList",ricettaRepository.findAll());
         return "show";
     }
+
+@GetMapping("/create")
+public String crea() {
+    return "create";
+}
+
+@GetMapping("/detail/{id}")
+public String dettaglio(@PathVariable Integer id,Model model) {
+    Optional<Ricetta> result=ricettaRepository.findById(id);
+        Ricetta ricetta= result.get();
+        model.addAttribute("ricetta",ricetta);
+        return "detail";
+}
 
 
 
--- src/main/java/com/learning/blogricette/repository/RicettaRepository.java
+++ src/main/java/com/learning/blogricette/repository/RicettaRepository.java
@@ -3,6 +3,8 @@ package com.learning.blogricette.repository;
 import com.learning.blogricette.model.Ricetta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ricettaRepository extends JpaRepository<Ricetta, Integer> {
+import java.util.Optional;
 
+public interface RicettaRepository extends JpaRepository<Ricetta,Integer> {
+    Optional<Ricetta> findById(Integer id);
 }
