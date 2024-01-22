0298372aa7641501b5795204237a37db7f6cafcd
LuigiantonioIacomino <iacominoluigi54@gmail.com>
2024-01-22 09:35:07 +0100

    Initial commit

---
+++ src/main/java/com/learning/blogricette/BlogRicetteApplication.java
@@ -0,0 +1,13 @@
+package com.learning.blogricette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogRicetteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogRicetteApplication.class, args);
+	}
+
+}
